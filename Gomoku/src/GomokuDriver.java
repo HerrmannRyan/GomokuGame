a5d676df4f3bf4f13bf3096b9be15ee3defd013e
ryan.herrmann <ryan.herrmann@mu-c1-17.mullen.pvt.k12.co.us>
2019-09-13 11:56:00 -0600

    Starting the array on the board

--- Gomoku/src/GomokuDriver.java
+++ Gomoku/src/GomokuDriver.java
@@ -1,12 +1,15 @@
+import java.util.concurrent.TimeUnit;
 
 public class GomokuDriver
 	{
 
-		public static void main(String[] args)
+		public static void main(String[] args) throws InterruptedException
 			{
 				
 				Introduction.introduceGame();
+				TimeUnit.SECONDS.sleep(3);
 				Display.createDisplay();
+				
 
 			}
 
--- Gomoku/src/Introduction.java
+++ Gomoku/src/Introduction.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class Introduction
 	{
-		static Scanner userInput = new Scanner (System.in);
+		static Scanner intInput = new Scanner (System.in);
+		static Scanner stringInput = new Scanner(System.in);
 		static int knowRules;
 		
 		public static void introduceGame()
@@ -10,23 +11,35 @@ public class Introduction
 			
 			greetPlayer();
 			if (knowRules == 1)
-				{
-					stateRulesOfGame();
-				}
+			{
+				stateRulesOfGame();
+			}
+			readyForBoard();
 		}
 		
 		private static void greetPlayer()
 		{
 			
 			System.out.println("Hello what is your name?");
-			String userName = userInput.nextLine();
-			System.out.println("Would you like to know the rules \n (1) Yes (2) No");
-			knowRules = userInput.nextInt();
+			String userName = stringInput.nextLine();
+			System.out.println("Welcome to Gomoku " + userName + ", would you like to know the rules \n (1) Yes (2) No");
+			knowRules = intInput.nextInt();
 			
 		}
 		
 		private static void stateRulesOfGame()
 		{
+			
+			System.out.println("The rules of the game are simple. State the grid of the board that you would like to fill. "
+					+ "\nIt is like tic tac toe, but it is five in a row. \nYou will be playing against me in this game... Got it?");
+			String enterToContinue = stringInput.nextLine();
+			System.out.println("Good Luck!");
+			
+		}
+		
+		private static void readyForBoard()
+		{
+			System.out.println("Creating Board...");
 			
 		}
 		
